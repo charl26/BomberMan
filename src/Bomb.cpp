2851c249ab43f061447757ee1ae8ef19f318fd0f
Franco <scheepersdev@gmail.com>
2017-08-15 09:45:59 +0200

    Created Entity Class - Bomb

---
+++ src/Bomb.cpp
@@ -0,0 +1,17 @@
+//
+// Created by Franco SCHEEPERS on 2017/08/15.
+//
+
+#include "Bomb.hpp"
+
+Bomb::Bomb(signed short int fuseTime) : fuseTime(fuseTime) {}
+
+signed short int Bomb::getFuseTime() const {
+    return fuseTime;
+}
+
+void Bomb::setFuseTime(signed short int fuseTime) {
+    Bomb::fuseTime = fuseTime;
+}
+
+Bomb::~Bomb() {}
