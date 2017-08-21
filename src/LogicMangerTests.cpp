c4c0a7e9b145a3731e471b88a5b2945933b133bd
chtheron <charltheron26@gmail.com>
2017-08-21 12:19:46 +0200

    changed test for logic manger to if else statements for easy readability

--- src/LogicMangerTests.cpp
+++ src/LogicMangerTests.cpp
@@ -12,14 +12,22 @@ bool testLogicManger() {
 
 	const std::vector<AbstractEntity *> &objects = eventManager.getObjects();
 	AbstractEntity *testObject = logicManager.retrieveObjectByCoordinates(2, 0);
-	std::cout << (nullptr != testObject ? "TEST1 PASSED : COLLIDED WITH A Object" : "TEST1 FAILED : PLAYER DIDN'T COLLIDED WITH A Object") << std::endl;
-	std::cout << (SOLIDBRICK == testObject->getType() ? "TEST2 PASSED : THE TYPE OF OBJECT THAT THE PLAYER COLLIDED WITH IS A WALL" : "TEST2 FAILED : PLAYER DIDN'T COLLIDED WITH A WALL") << std::endl;
+	if (nullptr != testObject) {
+		std::cout << "TEST1 PASSED : COLLIDED WITH A Object" << std::endl;
+	} else {
+		std::cout << "TEST1 FAILED : PLAYER DIDN'T COLLIDED WITH A Object" << std::endl;
+	}
+	if (SOLIDBRICK == testObject->getType()) {
+		std::cout << "TEST2 PASSED : THE TYPE OF OBJECT THAT THE PLAYER COLLIDED WITH IS A WALL" << std::endl;
+	} else {
+		std::cout << "TEST2 FAILED : PLAYER DIDN'T COLLIDED WITH A WALL" << std::endl;
+	}
 
 	testObject = logicManager.retrieveObjectByCoordinates(2, 1);
 	if (nullptr == testObject) {
-		std::cout << "TEST1 PASSED : PLAYER DIDN'T COLLIDED WITH A Object" << std::endl;
+		std::cout << "TEST3 PASSED : PLAYER DIDN'T COLLIDED WITH A Object" << std::endl;
 	} else {
-		std::cout << "TEST1 FAILED : COLLIDED WITH A Object" << std::endl;
+		std::cout << "TEST3 FAILED : COLLIDED WITH A Object" << std::endl;
 
 	}
 }
